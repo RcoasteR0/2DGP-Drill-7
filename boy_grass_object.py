b0133c7da9794086e9e36dd7b9e2703d08d24ca5
RcoasteR0 <st_anthony@naver.com>
2024-10-08 12:35:12 +0900

    boy_grass_object

---
+++ boy_grass_object.py
@@ -0,0 +1,71 @@
+from pico2d import *
+
+# Game object class here
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def update(self):self
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+         self.frame = (self.frame + 1) % 8
+         self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+def update_world():
+    grass.update()
+    boy.update()
+
+def render_world():
+    clear_canvas()
+    grass.draw()
+    boy.draw()
+    update_canvas()
+
+def reset_world():
+    global running
+    global grass
+    global boy
+
+    running = True
+    grass = Grass()
+    boy = Boy()
+
+open_canvas()
+
+# initialization code
+running = True
+grass = Grass()
+boy = Boy()
+
+# game main loop code
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
+
+# finalization code
+
+close_canvas()
